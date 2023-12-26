0d2ad900e09b80e67696bd914d3bfa39d97542fc
GhadeerHesham <154840228+GhadeerHesham@users.noreply.github.com>
2023-12-26 22:31:00 +0200

    responsive header js

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-// Initialize flatpickr for the date and time input field
-flatpickr("#bookingDateTime", {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-});
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
 
-function bookPhotographer(photographerId) {
-    // Get the selected date and time
-    var selectedDateTime = document.getElementById("bookingDateTime").value;
-
-    // Add logic to handle the booking process, e.g., show a confirmation modal
-    var photographerName = "Photographer " + photographerId;
-    var message = "You have successfully booked " + photographerName + " for " + selectedDateTime + ".";
-
-    document.getElementById("booking-message").innerHTML = message;
-    openModal();
-}
-
-function openModal() {
-    document.getElementById("booking-modal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("booking-modal").style.display = "none";
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
 }
