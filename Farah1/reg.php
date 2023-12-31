fc7ab75b398cefa5441d520ac5c0ab427d907f9e
farahmagdy1 <119798339+farahmagdy1@users.noreply.github.com>
2023-12-31 22:20:13 +0200

    Add files via upload

---
+++ Farah1/reg.php
@@ -0,0 +1,40 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "forgot_pass_";
+$conn = mysqli_connect($server, $user, $pass, $dbname);
+
+if (!$conn) {
+    echo "Failed";
+    die();
+} else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $Username = $_POST['Username'];
+        $Email = $_POST['Email'];
+        $Age = $_POST['Age'];
+        $Password = sha1($_POST['Password']);
+
+        $sql = "INSERT INTO userinfo (Username, Email, Age, Password) VALUES ('$Username','$Email','$Age','$Password')";
+
+        try {
+            if (mysqli_query($conn, $sql)) {
+                echo "success";
+                exit();
+            } else {
+                throw new Exception(mysqli_error($conn)); // Catch and throw the exception
+            }
+        } catch (Exception $e) {
+            if ($conn->errno == 1062) {
+                echo "Email or Username has been used before";
+                exit();
+            } else {
+                echo "Signup failed!";
+                exit();
+            }
+        }
+    }
+
+    mysqli_close($conn);
+}
+?>
