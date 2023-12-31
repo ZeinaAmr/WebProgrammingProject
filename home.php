f38d97261a56f516641fe7ed3ef97224b4d76b55
GhadeerHesham <154840228+GhadeerHesham@users.noreply.github.com>
2023-12-31 02:31:45 +0200

    Update home.php

--- home.php
+++ home.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
 
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -29,14 +30,128 @@
 
     </section>
 
+    <section class="home">
+
+    <div class="swiper home-slider">
+
+        <div class="swiper-wrapper">
+            
+            <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+                <div class="content">
+                    <span>where all memories are captured</span>
+                    <h3>creating visual heirlooms</h3>
+                    <a href="categories.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+                <div class="content">
+                    <span>where all memories are captured</span>
+                    <h3>Expertise meets creativity</h3>
+                    <a href="categories.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+                <div class="content">
+                    <span>where all memories are captured</span>
+                    <h3>The beauty of life, captured</h3>
+                    <a href="categories.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+    </div>
+
+    </section>
+
+    <section class="home-about">
+
+        <div class="image">
+            <img src="images/about-img.jpg" alt="about image">
+        </div>
+
+        <div class="content">
+            <h3>about</h3>
+            <p>Welcome to the Captured Moments Gallery, a breathtaking online showcase of extraordinary photographs captured by talented photographers from around the world. Immerse yourself in a visual journey where every click tells a unique story and each image is a testament to the artistry of our featured photographers.</p>
+            <a href="about.php" class="btn">read more</a>
+        </div>
+
+    </section>
+
+    <!-- categories-->
+    <!-- photographers-->
+
+    <section class="home-offer">
+
+    <div class="content">
+            <h3>Book Your Photographer Today and Enjoy Up to 30% Off!</h3>
+            <h4>🎉 New Year's Exclusive Photography Offer! 📸</h4>
+            <p>Capture the Moments, Cherish the Memories! 🌟 <br>
+            As we bid farewell to the old and welcome the new, we're thrilled to present an exclusive New Year's offer on our exceptional photography services! 🎊 <br>
+            <b>📆 Offer Period: January 1, 2023, to January 31, 2023</b></p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+
+    </section>
+
+       
+
+    <section class="footer">
+
+        <div class="box-container">
+            
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="categories.php"><i class="fas fa-angle-right"></i>Categories</a>
+                <a href="photographers.php"><i class="fas fa-angle-right"></i>Photographers</a>
+                <a href="photographers.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>Ask Question</a>
+                <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Terms of Use</a>
+            </div>
+
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+02 0123-456-7890</a>
+                <a href="#"><i class="fas fa-envelope"></i>capture@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>Smart Village, Kerdasa, Giza Governorate - 3650106</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
+                <a href="#"><i class="fab fa-whatsapp"></i>whatsapp</a>
+            </div>
+
+
+        </div>
+
+        <div class="credit">created by <span>Zeina Amr, Ghadeer Hesham, Farah Magdy</span></div>
+
+    </section>
 
 
 
 
 
 
-
-<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="script.js"></script>
 
 
