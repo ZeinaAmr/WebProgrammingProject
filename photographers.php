b5657c4846227d45c6a9bf88b79c5aab0066a697
GhadeerHesham <154840228+GhadeerHesham@users.noreply.github.com>
2024-01-01 03:11:11 +0200

    photographers frontend

---
+++ photographers.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv= "X-UA_Copmatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+
+    <section class="header">
+        <a href="home.php" class="logo">Capture</a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="categories.php">Categories</a>
+            <a href="photographers.php">Photographers</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <div class="heading" style="background:url(images/photographers-header.jpg) no-repeat">
+        <h1>photographers</h1>
+    </div>
+
+<section class="photographers">
+    <section class="sec-01">
+
+        <div class="container">
+
+            <h2 class="main-title">Fashion Photographers</h2>
+            <div class="content">
+                <div class="image">
+                    <img src="images/fashion-img/fashion-ph.jpeg" alt="Deanna Williams">
+                </div>
+                <div class="text-box">
+                    <h3>Deanna Williams</h3>
+                    <p>Known for her ability to capture the essence of her subjects, Deanna brings a unique blend of creativity and precision to every project. From the graceful flow of fabric to the subtleties of expression, her photographs showcase the beauty and authenticity inherent in each fashion moment.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+
+    </section>
+    <section class="sec-02">
+        <div class="container">
+
+            <div class="content">
+                <div class="image">
+                    <img src="images/fashion-img/fashion-ph-2.jpg" alt="Sophia Harper">
+                </div>
+                <div class="text-box">
+                    <h3>Sophia Harper</h3>
+                    <p>Meet Sophia Harper, an innovative and visionary fashion photographer whose lens transforms the world of style into a breathtaking visual narrative. With a passion for capturing the intersection of art and fashion, Sophia elevates each photograph to a realm of sophistication and elegance, creating images that resonate with both grace and boldness.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-03">
+        <div class="container">
+
+            <h2 class="main-title">Wedding Photographers</h2>
+            <div class="content">
+                <div class="image">
+                    <img src="images/wedding-img/wedding-ph-1.jpg" alt="Alex Turner">
+                </div>
+                <div class="text-box">
+                    <h3> Alex Turner</h3>
+                    <p>Meet Alex Turner, a passionate and talented wedding photographer with a flair for capturing timeless moments. With a keen eye for detail and a dedication to storytelling through his lens, Alex brings a unique perspective to every wedding he photographs. His warm and personable nature puts couples at ease, allowing him to capture authentic emotions and candid moments that reflect the true essence of their special day.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-04">
+        <div class="container">
+            <div class="content">
+                <div class="image">
+                    <img src="images/wedding-img/wedding-photographer-22.jpeg" alt="Emma Bennett">
+                </div>
+                <div class="text-box">
+                    <h3>Emma Bennett</h3>
+                    <p>Emma's photography is an artful blend of soft hues, natural light, and an intuitive understanding of her subjects. Her ability to connect with couples on a personal level allows her to document not only the events but also the emotions that make each wedding unique. Emma's work is a celebration of love in all its forms, portraying the beauty of commitment and the subtleties of human connection.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-05">
+        <div class="container">
+
+            <h2 class="main-title">Portrait Photographers</h2>
+            <div class="content">
+                <div class="image">
+                    <img src="images/portrait-img/portrait-ph-1.jpg" alt="Daniel Reynolds">
+                </div>
+                <div class="text-box">
+                    <h3>Daniel Reynolds</h3>
+                    <p>With a keen eye for lighting and composition, Daniel crafts portraits that are not just images but expressive works of art. His portfolio showcases a diverse range of subjects, from captivating environmental portraits to intimate close-ups, each revealing the depth and character of the individuals he photographs.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-06">
+        <div class="container">
+            <div class="content">
+                <div class="image">
+                    <img src="images/portrait-img/portrait-ph-2.jpg" alt="Olivia Mitchell">
+                </div>
+                <div class="text-box">
+                    <h3>Olivia Mitchell</h3>
+                    <p>With a distinctive style that combines elegance and modern flair, Olivia crafts portraits that go beyond mere images—they are reflections of the unique stories etched into the faces of her subjects. Her portfolio is a testament to her versatility, showcasing everything from striking studio portraits to intimate outdoor sessions, each one capturing the essence of the person in front of her lens.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-07">
+        <div class="container">
+
+            <h2 class="main-title">Wildlife Photographers</h2>
+            <div class="content">
+                <div class="image">
+                    <img src="images/wildlife-img/WL-1.jpg" alt="Adrian Turner">
+                </div>
+                <div class="text-box">
+                    <h3>Adrian Turner</h3>
+                    <p>With a keen understanding of wildlife behavior and habitats, Adrian ventures into the heart of nature to document its diverse inhabitants. His portfolio is a testament to the remarkable diversity of life, from the majestic movements of big cats on the prowl to the delicate dance of exotic birds in flight. Each photograph is a window into the untamed, showcasing the profound and often elusive moments that make the animal kingdom so mesmerizing.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-08">
+        <div class="container">
+            <div class="content">
+                <div class="image">
+                    <img src="images/wildlife-img/WL-2.jpg" alt="Hiroshi Tanaka">
+                </div>
+                <div class="text-box">
+                    <h3>Hiroshi Tanaka</h3>
+                    <p>Hiroshi's photographic journeys take him across continents, from dense rainforests to expansive plains, where he patiently waits to document extraordinary moments in the lives of wild creatures. His portfolio is a testament to the resilience of nature, featuring powerful images of magnificent predators in their habitats and intimate scenes of elusive species thriving in their ecosystems.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-09">
+        <div class="container">
+
+            <h2 class="main-title">Architecture Photographers</h2>
+            <div class="content">
+                <div class="image">
+                    <img src="images/arch-img/arch-1.jpg" alt="Gabriel Martinez">
+                </div>
+                <div class="text-box">
+                    <h3>Gabriel Martinez</h3>
+                    <p>Meet Gabriel Martinez, a distinguished architecture photographer whose lens transforms structures into visual masterpieces. Gabriel's passion for architectural photography is evident in his ability to capture the essence, intricacies, and design nuances of buildings with meticulous detail.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+    <section class="sec-10">
+        <div class="container">
+            <div class="content">
+                <div class="image">
+                    <img src="images/arch-img/arch-2.jpg" alt="Oliver Anderson">
+                </div>
+                <div class="text-box">
+                    <h3>Oliver Anderson</h3>
+                    <p>Known for his technical proficiency and a deep appreciation for the built environment, Oliver seamlessly combines precision with an artistic touch. Whether highlighting the grandeur of cityscapes or the intricate details of architectural elements, Oliver Anderson's work is a testament to the beauty and significance of the structures that shape our world.</p>
+                </div>
+            </div>
+
+            <div class="media-icons">
+                <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="icon"><i class="fab fa-linkedin"></i></a>
+                <a href="#" class="icon"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <a href="#" class="btn">portfolio</a>
+
+        </div>
+    </section>
+</section>
+
+
+
+
+
+
+<section class="footer">
+
+    <div class="box-container">
+        
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+            <a href="categories.php"><i class="fas fa-angle-right"></i>Categories</a>
+            <a href="photographers.php"><i class="fas fa-angle-right"></i>Photographers</a>
+            <a href="photographers.php"><i class="fas fa-angle-right"></i>Book</a>
+        </div>
+
+
+        <div class="box">
+            <h3>Extra Links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i>Ask Question</a>
+            <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>Terms of Use</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a href="#"><i class="fas fa-phone"></i>+02 0123-456-7890</a>
+            <a href="#"><i class="fas fa-envelope"></i>capture@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i>Smart Village, Kerdasa, Giza Governorate - 3650106</a>
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
+            <a href="#"><i class="fab fa-whatsapp"></i>whatsapp</a>
+        </div>
+
+
+    </div>
+
+<div class="credit">created by <span>Zeina Amr, Ghadeer Hesham, Farah Magdy</span></div>
+
+</section>
+
+
+
+
+<script src="https://unpkg.com/scrollreveal"></script>
+<script src="script.js"></script>
+<script>
+    ScrollReveal({
+        reset: true,
+        distance: '60px',
+        duration: 2500,
+        delay: 400
+    });
+    ScrollReveal().reveal('.main-title',{delay:400, origin: 'left'});
+    ScrollReveal().reveal('.sec-01 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-02 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-03 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-04 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-05 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-06 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-07 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-08 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-09 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.sec-10 .image',{delay:600, origin: 'bottom'});
+    ScrollReveal().reveal('.text-box',{delay:700, origin: 'right'});
+    ScrollReveal().reveal('.media-icons i',{delay:500, origin: 'bottom', interval: 100});
+</script>
+
+
+</body>
+</html>
