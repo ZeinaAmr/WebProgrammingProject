723902248ad1d3720ca6f6f2159e20f1f65884c2
GhadeerHesham <154840228+GhadeerHesham@users.noreply.github.com>
2024-01-01 03:27:07 +0200

    about the website

---
+++ about.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv= "X-UA_Copmatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+    <section class="header">
+        <a href="home.php" class="logo">Capture</a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="categories.php">Categories</a>
+            <a href="photographers.php">Photographers</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <div class="heading" style="background:url(images/about-page.jpg) no-repeat">
+        <h1>about</h1>
+    </div>
+
+
+    <section class="about">
+        <section class="start">
+            <div class="image">
+                <img src="images/about-img.jpg" alt="about image">
+            </div>
+
+
+            <div class="content">
+                <h3>why choose us?</h3>
+                <p>At capture, we understand that you have choices when it comes to photography industry. Here are compelling reasons why we stand out and why you should choose us:</p>
+            </div>
+        </section>
+
+            
+        <section class="points">
+            <div class="icons-container">
+                    <div class="icons">
+                        <h3><i class="fas fa-trophy"></i> Expertise and Experience:</h3>
+                        <p>With a team of seasoned professionals in the field, we bring a wealth of experience and expertise. Our track record speaks for itself, and we are committed to delivering exceptional photos tailored to your needs.</p>
+                    </div>
+                    <div class="icons">
+                        <h3><i class="fas fa-medal"></i> Quality Beyond Compromise:</h3>
+                        <p>We pride ourselves on delivering nothing but the highest quality. From the materials we use to the craftsmanship in our work, we go the extra mile to ensure excellence in every aspect of what we do.</p>
+                    </div>
+                    <div class="icons">
+                        <h3><i class="fas fa-headset"></i> Customer-Centric Approach:</h3>
+                        <p>Your satisfaction is our priority. We believe in building lasting relationships with our clients by putting your needs first. Our dedicated support team is always ready to assist you and ensure a smooth and enjoyable experience.</p>
+                    </div>
+                    <div class="icons">
+                        <h3><i class="fas fa-camera"></i> Innovative Solutions:</h3>
+                        <p>Embracing innovation is at the core of our philosophy. We continually strive to bring you cutting-edge photos that align with the latest trends and technological advancements in the industry.</p>
+                    </div>
+            </div>
+        </section>
+
+    </section>
+
+
+
+
+
+    <section class="footer">
+
+        <div class="box-container">
+            
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="categories.php"><i class="fas fa-angle-right"></i>Categories</a>
+                <a href="photographers.php"><i class="fas fa-angle-right"></i>Photographers</a>
+                <a href="photographers.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>Ask Question</a>
+                <a href="#"><i class="fas fa-angle-right"></i>About Us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Terms of Use</a>
+            </div>
+
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+02 0123-456-7890</a>
+                <a href="#"><i class="fas fa-envelope"></i>capture@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>Smart Village, Kerdasa, Giza Governorate - 3650106</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow Us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
+                <a href="#"><i class="fab fa-whatsapp"></i>whatsapp</a>
+            </div>
+
+
+        </div>
+
+        <div class="credit">created by <span>Zeina Amr, Ghadeer Hesham, Farah Magdy</span></div>
+
+    </section>
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="script.js"></script>
+
+
+</body>
+</html>
